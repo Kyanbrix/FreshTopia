8a080d3384a5b4364b9c65e7943b54171a1a1918
Kian <kyanbrix@yahoo.com>
2025-03-14 03:10:31 +0800

    Change structure, added a class to be usable for future use

--- gsm_module/send_message.py
+++ gsm_module/send_message.py
@@ -1,15 +1,22 @@
 from serial import Serial
 
 import time
-ser = Serial('/dev/ttyS0', baudrate=115200, timeout=1)
 
 
-def send_sms(text, phone_number):
-    ser.write(b'AT+CMGF=1\r\n')
-    time.sleep(1)
-    ser.write(f'AT+CMGS="{phone_number}"\r\n'.encode())
-    time.sleep(1) # Blocking change to async or thread
-    ser.write((text + "\x1A".encode()))
-    
+class SMS:
+
+    def __init__(self,serial_port,phone_number):
+
+        self.port = serial_port
+        self.phone_number = phone_number
+
+    def send_sms(self,text_msg):        
+        ser = Serial(self.port, baudrate=115200, timeout=1)
+        ser.write(b'AT+CMGF=1\r\n')
+        time.sleep(1)
+        ser.write(f'AT+CMGS="{self.phone_number}"\r\n'.encode())
+        time.sleep(1)
+        ser.write((text_msg + "\x1A".encode()))
+
 
 
