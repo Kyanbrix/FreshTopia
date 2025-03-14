1ac13618976dbcb174297ac527e8bf4e2bc3e896
Kian <kyanbrix@yahoo.com>
2025-03-15 02:16:51 +0800

    Added a DateFormatter Class

--- controller/date_logic.py
+++ controller/date_logic.py
@@ -1,6 +1,8 @@
 
 from dateutil.relativedelta import relativedelta
 
+from datetime import datetime
+
 class AppDate:
 
     def __init__(self, date):
@@ -14,3 +16,23 @@ class AppDate:
 
     def add_years(self, years):
         return self.current_date + relativedelta(years=years)
+    
+
+class DateFormatter:
+
+
+    def __init__(self, datetime : str):
+        self.datetime = datetime
+    
+
+    def format_datetime_to_fullname(self):
+        datetime_obj = datetime.strptime(self.datetime,'%Y-%m-%d')
+
+        return datetime_obj.date().strftime('%B %d, %Y')
+
+
+
+
+
+
+        
